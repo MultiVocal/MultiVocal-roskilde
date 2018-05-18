fa022f79059e6f169d4b013f62ef6d54a9fa458d
Raspberry Pi <raspberry@multivocal.com>
2018-05-18 12:45:55 +0000

    mono recording works

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -24,7 +24,7 @@ void ofApp::setup() {
 #elif __arm__
   audioRecorder.setFfmpegLocation(
       ofFilePath::getAbsolutePath("ffmpeg/ffmpeg_arm"));
-  inputChannels = 2;
+  inputChannels = 1;
   outputChannels = 0;
   soundStream.setDeviceID(2);
 #endif
